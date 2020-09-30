bf72ad0c9c5065f679dc6c4032b1496cfc8e5b60
Michael Whatcott <mdwhatcott@gmail.com>
2020-09-30 17:13:29 -0600

    Mark Assert method as a Helper.

--- t.go
+++ t.go
@@ -12,9 +12,10 @@ type TT struct{ *testing.T }
 // Assert compares expected and actual and calls t.Error with
 // a full report of any discrepancy between them.
 func (this TT) Assert(expected, actual interface{}, options ...Option) bool {
+	this.T.Helper()
 	ok, report := Compare(expected, actual, options...)
 	if !ok {
-		this.Error(report)
+		this.T.Error(report)
 	}
 	return ok
 }
